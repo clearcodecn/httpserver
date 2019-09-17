f77e576c8ec3b944ec71e1e8f9653be97556a7d2
mrj <git@clearcode.cn>
2019-09-17 19:52:05 +0800

    init

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+	"os"
+)
+
+var (
+	addr string
+	staticDir string
+)
+
+func init() {
+	flag.StringVar(&addr,"addr",":8080","address")
+	flag.StringVar(&staticDir,"s","/static","static dir")
+}
+func main() {
+	flag.Parse()
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		name, _ := os.Hostname()
+		writer.Write([]byte(name))
+	})
+
+	http.Handle("/static",http.FileServer(http.Dir(statidDir)))
+
+
+	http.ListenAndServe(addr, nil)
+}
